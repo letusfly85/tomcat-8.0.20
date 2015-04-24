822196deae261bc91298a645a3a4de25623a1723
letusfly85 <letusfly95@gmail.com>
2015-04-24 09:16:40 +0000

    java8

--- tomcat.rb
+++ tomcat.rb
@@ -3,9 +3,14 @@ package "software-properties-common" do
 end
 
 execute "add ppa:webupd8team/java" do
-	command "add-apt-repository ppa:webupd8team/java"
+	command <<-EOF
+		echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+		add-apt-repository -y ppa:webupd8team/java
+		apt-get update
+	EOF
 end
 
 package "oracle-java8-installer" do
 	action :install
+	options "--force-yes"
 end
