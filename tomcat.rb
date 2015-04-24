4649d1ee1ca9479f3066293889f96dda7e6ce292
letusfly85 <letusfly95@gmail.com>
2015-04-24 09:32:27 +0000

    add tomcat

--- tomcat.rb
+++ tomcat.rb
@@ -14,3 +14,13 @@ package "oracle-java8-installer" do
 	action :install
 	options "--force-yes"
 end
+
+execute "wget tomcat" do
+	tar_file="apache-tomcat-8.0.21.tar.gz"
+	command <<-EOF
+		wget http://www.apache.org/dist/tomcat/tomcat-8/v8.0.21/bin/#{tar_file}
+		mkdir -p /var/local
+		tar xfz #{tar_file} -C /var/local
+	EOF
+end
+
